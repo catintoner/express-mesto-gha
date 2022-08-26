dbbf38dec3380091632b2d7284092db30fa4e6de
Dmitry Lukyanov <lukyanovd.a@yandex.ru>
2022-08-27 00:41:49 +0300

    feature: add validity and regular

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { Joi, celebrate, errors } = require('celebrate');
 
 const auth = require('./middlewares/auth');
 
@@ -23,18 +24,66 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  login,
+);
+
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  createUser,
+);
 
 app.use(cookieParser());
 
-app.use('/users', auth, router);
+app.use(
+  '/users',
+  celebrate({
+    params: Joi.object().keys({
+      userId: Joi.string().alphanum().length(24),
+    }),
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string(),
+    }),
+  }),
+  auth,
+  router,
+);
 
-app.use('/cards', auth, cardRouter);
+app.use(
+  '/cards',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().alphanum().length(24),
+    }),
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      link: Joi.string().required().min(2).max(30),
+    }),
+  }),
+  auth,
+  cardRouter,
+);
 
 app.use('*', (request, response, next) => {
   next(new NotFoundError('Запрашиваемая страница не найдена'));
 });
+
+app.use(errors());
 
 app.use((err, request, response, next) => {
   if (err.statusCode) {
--- models/user.js
+++ models/user.js
@@ -43,6 +43,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     required: true,
+    validate: {
+      validator(v) {
+        return /https?:\/\/(www.)?\w#?/.test(v);
+      },
+    },
   },
 }, { versionKey: false });
 
